33fc79d82a40be87c80f38da044e40ef77de291f
Bastian Bohac <bastian.bohac@gmail.com>
2023-06-13 23:43:48 +0200

    pong-1
    https://github.com/games50/pong/tree/master/pong-1

--- main.lua
+++ main.lua
@@ -1,11 +1,20 @@
+push = require 'push'
+
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
 
 --[[
     Runs when the game first starts up, only once; used to initialize the game.
 ]]
 function love.load()
-    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+    -- use nearest-neighbor filtering on upscaling and downscaling to prevent blurring of text 
+    -- and graphics; try removing this function to see the difference!
+    love.graphics.setDefaultFilter("nearest", "nearest")
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
         vsync = true
@@ -13,13 +22,22 @@ function love.load()
 end
 
 --[[
+    Keyboard handling, called by LÖVE2D each frame; 
+    passes in the key we pressed so we can access.
+]]
+function love.keypressed(key)
+    if key == "escape" then
+        love.event.quit()
+    end
+end
+
+--[[
     Called after update by LÖVE2D, used to draw anything to the screen, updated or otherwise.
 ]]
 function love.draw()
-    love.graphics.printf(
-        "Hello Pong!",              -- text to render
-        0,                          -- starting x (0 since we're going to center it based on width)
-        WINDOW_HEIGHT / 2 - 6,      -- starting Y (halfway down the screen)
-        WINDOW_WIDTH,               -- number of pixels to center within (the entire screen here)
-        "center")                   -- alignment mode, can be "center", "left" or "right"
+    push:apply("start")
+
+    love.graphics.printf("Hello Pong!", 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, "center")
+
+    push:apply("end")
 end
